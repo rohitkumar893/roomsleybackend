db6f615cc3080090e2238bf2702ff36fcf5621e4
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-05-15 11:51:06 +0530

    updated listing api

--- controller/listings/listings.js
+++ controller/listings/listings.js
@@ -4,3 +4,11 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors());
+
+app.get('/listings', (req,res) => {
+    const data = req.body;
+    console.log(data);
+    res.json({message:'got it', data})
+});
+
+module.exports = listings;
