920e5da3311c7ef6268b5bac935083235382fa2b
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-06-03 22:48:52 +0530

    updated frontend link

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://roomsley.vercel.app",
   methods:["GET","POST","PUT","DELETE"]
 }));
 
