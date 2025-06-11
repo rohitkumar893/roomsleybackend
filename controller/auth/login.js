059c48870e400f1aa31bc61fbd867683b88aef7b
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-06-11 08:09:57 +0530

    removed unnecessary code

--- controller/auth/login.js
+++ controller/auth/login.js
@@ -1,8 +1,5 @@
 const User = require('../../models/User.model.js')
 const jwt = require('jsonwebtoken')
-// const{ loginValidation }=require("../../services/validation_schema_login")
-
-// const JWT_USER_PASSWORD = "rohitrohit"
 
 const login= async (req, res) => {
     try {
@@ -17,28 +14,12 @@ const login= async (req, res) => {
 
       const existingUser = await User.findOne({ username });
       if (existingUser && existingUser.password === password) {
-        //  token = jwt.sign(
-        //     {
-        //         _id: existingUser._id
-        //     },
-        //     JWT_USER_PASSWORD
-        // );
-        // console.log(token);
         return res.json({
         message: "Login successful"
       });
       } else {
         return res.status(400).json({ message: "Invalid username or password" });
       }
-
-      // res.json({ 
-      //   message: "Login successful",
-      //   token
-    // });
-
-    // if (existingUser) {
-    //   return res.status(400).json({ message: "Login successful" });
-    // }
 
     } catch (error) {
       console.error("Server Error:", error);
